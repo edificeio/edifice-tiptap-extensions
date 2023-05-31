126af5cbf3684be9dfe92a5d80de18d639717294
Clement Creusat <clement.creusat@gmail.com>
2023-05-31 20:05:23 +0200

    fix: ignore eslintrc file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {},
+  ignorePatterns: ['.eslintrc.cjs'],
 };
