07e5ea1af984fd0abae7a644ff427326cd4c4dcb
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-06-28 11:20:42 +0000

    feat(extension): Add speechsynthesis extension

---
+++ packages/extension-speechsynthesis/src/SpeechSynthesis.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {Node} from '@tiptap/core'
+
+export interface SpeechSynthesisOptions {
+  lang: string,
+  pitch: number,
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    speechSynthesis: {
+      startSpeechSynthesis: () => ReturnType,
+      stopSpeechSynthesis: () => ReturnType,
+    }
+  }
+}
+
+class SS_Node<O = any, S = any> extends Node<O, S> {
+  static create<O = any, S = any>(config?: any) {
+    return Node.create(config) as SS_Node<O, S>;
+  }
+};
+
+const SpeechSynthesis = SS_Node.create<SpeechSynthesisOptions>({
+  name: 'speechSynthesis',  
+  addOptions() {
+    return {
+      lang: 'fr-FR',
+      pitch: 1,
+    }
+  },
+  addCommands() {
+    return {
+      startSpeechSynthesis: () => ({ commands }) => {
+        this.speechSynthesis = new SpeechSynthesisUtterance()
+        this.speechSynthesis.lang = this.options.lang
+        this.speechSynthesis.pitch = this.options.pitch
+        this.speechSynthesis.text = this.editor.getText()
+
+        window.speechSynthesis.speak(this.speechSynthesis)
+        return commands
+      },
+      stopSpeechSynthesis: () => ({ commands }) => {
+        window.speechSynthesis.cancel()
+        return commands
+      }
+    }
+  },
+})
+
+export {SpeechSynthesis}
+
+export default SpeechSynthesis
