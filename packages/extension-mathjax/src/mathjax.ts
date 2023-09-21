225ba807e4030b3d40fb7cd1bb1419076eaffab6
Junior Bernard <junior.bernard@opendigitaleducation.com>
2023-09-21 17:42:45 +0200

    feat: #WB-2164, add extension to convert old MathJax content

---
+++ packages/extension-mathjax/src/mathjax.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Node } from '@tiptap/core';
+
+
+const MathJaxNode = Node.create({
+  name: 'mathjaxnode',
+  group: 'inline',
+  inline: true,
+  atom: false,
+  selectable: true,
+
+
+  parseHTML() {
+    return [
+      {
+        tag: 'mathjax',
+      },
+    ];
+  },
+
+  addAttributes() {
+    return {
+      equation: {
+        default: null,
+        parseHTML: (element: any) => {
+          const equationNode = [...element.childNodes]
+            .filter(child => child.nodeType === 3)
+            .filter(child => child.data.indexOf('begin{equation') >= 0)[0]
+          if (equationNode) {
+            return equationNode.nodeValue
+          } else {
+            return null;
+          }
+        }
+      }
+    }
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    const equation = (HTMLAttributes.equation || '')
+      .replaceAll(/\\begin{equation}\s*\n?\s*{/mg, '$')
+      .replaceAll(/}\n?\s*\\end{equation}/mg, '$');
+    return ['span', {}, equation];
+  },
+})
+
+export { MathJaxNode };
+export default MathJaxNode;
