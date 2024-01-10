b4c8f333a4448f413b361cb7ed9d4daad6896b04
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-01-10 14:53:49 +0100

    feat: #WB-2472, style the produced table

--- packages/extension-templates/src/templates.ts
+++ packages/extension-templates/src/templates.ts
@@ -41,20 +41,19 @@ const TemplatesNode = Node.create({
   renderHTML({ HTMLAttributes }) {
     const columns = HTMLAttributes.columns || [];
     const titles = columns.map((column) => {
-      return ['th', {}, column.title];
-    });
-    const cells = columns.map((column) => {
-      if (column.image) {
-        return ['td', {}, ['img', { src: column.image }]];
+      if(column.title) {
+        return ["td", {'class': 'title'}, ["h2", {}, column.title]];
       } else {
-        return ['td', {}, column.content];
+        return ["td", {'class': 'image', rowspan: 2}, ["img", { src: column.image }]];
       }
     });
+    const cells = columns.filter(c => !!c.content).map(column => {
+      return ["td", {'class': 'text'}, column.content];
+    });
     return [
-      'table',
-      {},
-      ['thead', {}, ['tr', {}, ...titles]],
-      ['tbody', {}, ['tr', {}, ...cells]],
+      "table",
+      {'class': 'template'},
+      ["tbody", {}, ["tr", {}, ...titles], ["tr", {}, ...cells]]
     ];
   },
 });
