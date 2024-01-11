97f1f760cfcd015c1687cffb9c70338b0e9bbee4
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-01-11 15:02:30 +0100

    feat: #WB-2473, convert old info and warning blocks

--- packages/extension-alert/src/alert.ts
+++ packages/extension-alert/src/alert.ts
@@ -1,31 +1,30 @@
 import { Mark } from '@tiptap/core';
 
 const AlertsNode = Mark.create({
-  
-  name: "alerts",
+  name: 'alerts',
 
   parseHTML() {
     return [
       {
-        tag: "p[class=info]"
+        tag: 'p[class=info]',
       },
       {
-        tag: "p[class=warning]"
-      }
+        tag: 'p[class=warning]',
+      },
     ];
   },
   addAttributes() {
     return {
       type: {
         default: 'info',
-        parseHTML: (element) => element.getAttribute("class")
-      }
+        parseHTML: (element) => element.getAttribute('class'),
+      },
     };
   },
   renderHTML({ HTMLAttributes }) {
     const type = HTMLAttributes.type;
-    return ["div", { "class": type }, 0];
-  }
+    return ['div', { class: type }, 0];
+  },
 });
 
 export { AlertsNode };
