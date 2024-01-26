b5571c4898496c4a50c1ceb5a264cf447527b85e
Romu-C <romuald.coulaud@gmail.com>
2024-01-26 11:21:46 +0100

    fix: #WB-2502 Reduce default size for images

--- packages/extension-image/src/CustomImage.ts
+++ packages/extension-image/src/CustomImage.ts
@@ -68,7 +68,8 @@ export const CustomImage = Image.extend<CustomImageOptions>({
         parseHTML: (element) => element.getAttribute('title'),
       },
       width: {
-        default: '350',
+        // The default value is small to have a got retrocompatibility with the panda smiley #WB-2502
+        default: '80',
         renderHTML: (attributes) => {
           return {
             width: parseInt(attributes.width),
